3a1f7eb7596617f757607b39b89f8b9f84d450bf
Vinícius Kury <71557664+nikbott@users.noreply.github.com>
2024-02-16 16:17:12 -0300

    Add pip to installed packages
    Signed-off-by: Vinícius Kury <71557664+nikbott@users.noreply.github.com>

--- postinstall.sh
+++ postinstall.sh
@@ -16,7 +16,7 @@ ip link set `ip --brief link | awk '$1 !~ "lo|vir|wl" { print $1}'` up
 sleep 10
 apt update  # To get the latest package lists
 apt upgrade -y
-apt install -y build-essential dcraw freecad gimp inkscape libraw-bin remmina whois # commonly used packages
+apt install -y build-essential dcraw freecad gimp inkscape libraw-bin python-pip remmina whois # commonly used packages
 snap refresh
 snap install firefox
 
