bb515edeac2f410e7107bdbb7e28cd4ba64bf8f6
Vinícius Kury <71557664+nikbott@users.noreply.github.com>
2024-10-18 11:19:59 -0300

    Extract version to a variable
    DRY
    
    Signed-off-by: Vinícius Kury <71557664+nikbott@users.noreply.github.com>

--- test_autoinstall.sh
+++ test_autoinstall.sh
@@ -2,20 +2,25 @@
 
 set -e
 
+# Define version variable
+VERSION="24.04.1"
+
 # Install emulator
 sudo apt update
-sudo apt install qemu-system-x86
+sudo apt install -y qemu-system-x86
 
 # Start web server for autoinstall file.
-# Ideally we should boot from netboot.
+# Ideally, we should boot from netboot.
 python3 -m http.server 3003 & echo $! > ./autoinstall_server_test.pid
 
-# Create disk image and boot vm in UEFI mode, instructing the kernel to autoinstall.
+# Create disk image and boot VM in UEFI mode, instructing the kernel to autoinstall.
 qemu-img create -f qcow2 autoinstall.img 60G
-kvm -no-reboot -cpu host -m 4096 -bios OVMF.fd -drive file=autoinstall.img,if=virtio \
-	-cdrom /var/www/html/ubuntu-22.04.3-live-server-amd64.iso \
-	-kernel /srv/tftp/vmlinuz -initrd /srv/tftp/initrd \
-	-append 'cloud-config-url=http://_gateway:3003/autoinstall.yaml autoinstall'
+kvm -no-reboot -cpu host -m 4096 -bios OVMF.fd \
+	-drive file=autoinstall.img,if=virtio \
+	-cdrom /var/www/html/ubuntu-${VERSION}-live-server-amd64.iso \
+	-kernel /srv/tftp/vmlinuz \
+	-initrd /srv/tftp/initrd \
+	-append "cloud-config-url=http://_gateway:3003/autoinstall.yaml autoinstall"
 
 # Kill web server as it's not needed.
 kill $(cat ./autoinstall_server_test.pid)
